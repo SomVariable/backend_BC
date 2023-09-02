9666d9e7109bc8562efcb2ae770cdbc886eed895
SomVariable <79557545+SomVariable@users.noreply.github.com>
2023-09-02 17:55:24 +0300

    added response DTOs to PInterestModule

---
+++ src/api/professional-interest/dto/ok-response/ok.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Area, ContentItem, Education, News, ProfessionalInterest } from "@prisma/client";
+import { P_INTEREST_OK, P_INTEREST_EXAMPLES } from "../../constants/professional-interest.constants";
+
+export class PInterestOkResponse {
+    @ApiProperty({
+        type: P_INTEREST_OK.OK,
+        default: P_INTEREST_OK.OK,
+        enum: P_INTEREST_OK
+    })
+
+    message: P_INTEREST_OK.OK;
+
+    @ApiProperty({
+        example: P_INTEREST_EXAMPLES
+    })
+    data: ProfessionalInterest
+
+}
---
+++ src/api/professional-interest/dto/professional-interest-bad-request-error.dto.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { AppErrorResponse } from "src/common/dto/errors.dto";
+import { P_INTEREST_BAD_REQUEST } from "../constants/professional-interest.constants";
+
+export class PInterestBadRequestErrorResponse extends AppErrorResponse {
+    @ApiProperty({
+        type: P_INTEREST_BAD_REQUEST,
+        enum: P_INTEREST_BAD_REQUEST
+    })
+    message: string;
+  }
