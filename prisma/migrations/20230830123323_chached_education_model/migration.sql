a15c05a12455336c0076c553d4d08356c38f9ff7
SomVariable <79557545+SomVariable@users.noreply.github.com>
2023-10-02 08:36:01 +0300

    delete prisma migration from branch

--- prisma/migrations/20230830123323_chached_education_model/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Education" ALTER COLUMN "graduationYear" SET DATA TYPE TEXT,
-ALTER COLUMN "studyYear" SET DATA TYPE TEXT;
--- prisma/migrations/20231002050950_update_educ_transl/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "EducationTranslation" DROP CONSTRAINT "EducationTranslation_educationId_fkey";
-
--- AddForeignKey
-ALTER TABLE "EducationTranslation" ADD CONSTRAINT "EducationTranslation_educationId_fkey" FOREIGN KEY ("educationId") REFERENCES "Education"("id") ON DELETE CASCADE ON UPDATE CASCADE;
